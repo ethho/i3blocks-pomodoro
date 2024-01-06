5b70916f7782c12094e55d882de3002aa4c4b3b4
Ethan Ho <ethan.n.ho@gmail.com>
2024-01-05 19:06:30 -0700

    Working with interval=1

--- clock.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import time
-
-while True:
-    print("True", flush=True)
-    time.sleep(1)
--- continuous_wc.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import sys
-
-def main():
-    for line in sys.stdin:
-        print(line, flush=True, end='')
-
-if __name__=='__main__':
-    main()
